830f12f3a780ca6c4dfcd22204f27760e97a35d4
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-07-09 11:08:51 -0700

    created user model and set up user authentication routes/controller

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ connectDB();
 // Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 const app = express();
 
@@ -40,6 +41,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 // If we want to be able to use error middleware in the bootcamps controller methods, it HAS to be AFTER the above line, since middleware is run in a linear order
 app.use(errorHandler);
